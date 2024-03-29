12e502c9a31f555134cd52fe9bd87bd6ba225e00
Thomas <thomascode92@hotmail.com>
2024-03-29 14:38:17 +0100

    Installation Script for Discord

---
+++ run_once_install-discord.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Download the Discord .deb package
+cd ~/Downloads
+wget https://discord.com/api/download?platform=linux&format=deb
+
+# Install the Discord .deb package
+sudo dpkg -i download?platform=linux
