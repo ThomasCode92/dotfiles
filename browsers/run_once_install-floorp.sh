281b4f095aeaea473bfac1c030d8ddeebc0e592f
Thomas <thomascode92@hotmail.com>
2024-06-04 15:58:00 +0200

    Install Floorp Browser

---
+++ browsers/run_once_install-floorp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd ~/Downloads
+
+curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
+sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
+
+sudo apt update
+sudo apt install floorp
