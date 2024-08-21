766722e7c24b0c80e7d636e06db995631d0035bf
Thomas <thomascode92@hotmail.com>
2024-08-21 08:47:11 +0200

    Remove GameDev Scripts

--- gamedev/run_once_install-unity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ~/Downloads
-
-wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
-sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
-
-sudo apt update
-sudo apt-get install unityhub
