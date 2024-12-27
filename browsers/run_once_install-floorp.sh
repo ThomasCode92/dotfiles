702904da77f9114299e376845b4332e94fb7c565
Thomas <thomascode92@hotmail.com>
2024-12-27 16:10:20 +0100

    Update the Install Scripts for Browsers

--- browsers/run_once_install-floorp.sh
+++ browsers/run_once_install-floorp.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cd ~/Downloads
-
 curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
 sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
 
