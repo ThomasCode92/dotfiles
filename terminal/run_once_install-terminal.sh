0f813dff53291fb0eec70038385c587ccb1fbacf
Thomas <thomascode92@hotmail.com>
2025-01-19 12:07:37 +0100

    Restructure Terminal Scripts

--- terminal/run_once_install-terminal.sh
+++ terminal/run_once_install-terminal.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
 
-# Install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-
-# Install fish
+# Install fish, zsh and tmux
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
 sudo apt install tmux
 
-# Install curl
-sudo apt install curl
+# Install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
 
-# Install starship
-curl -sS https://starship.rs/install.sh | sh
-
-# Change default shell to fish
-chsh -s $(which fish)
-
-# Install asdf plugins
-mkdir -p ~/.config/fish/completions
-ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+# Change default shell to zsh
+chsh -s $(which zsh)
