a6bb09f61810ae3fdf300354c139e44f5d97e833
Thomas <thomascode92@hotmail.com>
2025-01-20 07:55:42 +0100

    Hard Reset

--- databases/run_once_install-dbeaver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
-sudo dpkg -i dbeaver-ce_latest_amd64.deb
--- terminal/run_once_install_starship.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Install starship
-curl -sS https://starship.rs/install.sh | sh
-
-# Install asdf plugins for fish
-mkdir -p ~/.config/fish/completions
-ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions
