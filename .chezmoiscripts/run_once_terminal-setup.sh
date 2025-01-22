ffe241a459231022e3ee6d1de62d3e0e3877bfb8
Thomas <thomascode92@hotmail.com>
2025-01-22 20:54:48 +0100

    Terminal Setup Script

---
+++ .chezmoiscripts/run_once_terminal-setup.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+## Package
+sudo apt install fzf
+sudo apt install fd-find
+sudo apt install bat
+
+## Install starship
+if ! command -v starship >/dev/null 2>&1; then
+    echo "Installing starship..."
+    curl -fsSL https://starship.rs/install.sh | bash -s -- -y
+else
+    echo "starship already installed, installation skipped"
+    echo "version: $(starship --version)"
+fi
+
+## Setup for bat
+if [ ! -e ~/.local/bin/bat ]; then
+    echo "Setting up bat..."
+    mkdir -p ~/.local/bin
+    ln -s /usr/bin/batcat ~/.local/bin/bat
+
+    mkdir -p "$(bat --config-dir)/themes"
+    cd "$(bat --config-dir)/themes"
+    curl -O https://github.com/folke/tokyonight.nvim/blob/main/extras/sublime/tokyonight_night.tmTheme bat cache --build
+    echo "bat configured and tokyonight_night theme set" 
+else
+    echo "bat already configured, configuration skipped"
+    echo "version: $(bat --version)"
+fi
+
+## Install asdf
+if [ ! -d ~/.asdf ]; then
+    echo "Installing asdf..."
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.15.0
+else
+    echo "asdf already installed, installation skipped"
+    echo "version: $(asdf --version)"
+fi
