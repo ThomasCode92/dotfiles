d9131dc736c59e412be7a35a8e43aeb85a486fba
Thomas <108367944+ThomasCode92@users.noreply.github.com>
2025-01-30 08:19:46 +0100

    Neovim from Scratch
    * Create the 'init.lua' file
    
    * Install 'lazy.vim' and a Color Scheme
    
    * Configure Telescoop
    
    * Configure Treesitter

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,23 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+require("config.lazy")
+
+vim.cmd[[colorscheme tokyonight]]
+
+-- telescoop
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+
+-- treesitter
+local config = require('nvim-treesitter.configs')
+config.setup({
+  ensure_installed = { "lua", "javascript" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
