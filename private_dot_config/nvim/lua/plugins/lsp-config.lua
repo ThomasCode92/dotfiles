32a93a8d861ece18f73226337a3e49e3e6322c7d
Thomas <thomascode92@hotmail.com>
2025-01-31 22:01:04 +0100

    Installing 'nvim-lsp-config'

--- private_dot_config/nvim/lua/plugins/lsp-config.lua
+++ private_dot_config/nvim/lua/plugins/lsp-config.lua
@@ -5,12 +5,19 @@ return {
       require("mason").setup()
     end
   },
-  { 
+  {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls" }
       })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
     end
   }
 }
