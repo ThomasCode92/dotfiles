e3e063187a2aa8abe0328896b5dc53e3334e4ecd
Thomas <thomascode92@hotmail.com>
2025-02-01 13:31:20 +0100

    Install 'stylue'

--- private_dot_config/nvim/lua/plugins/none-ls.lua
+++ private_dot_config/nvim/lua/plugins/none-ls.lua
@@ -4,8 +4,10 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua
-      }
+        null_ls.builtins.formatting.stylua,
+      },
     })
-  end
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
