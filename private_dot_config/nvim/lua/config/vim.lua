275895f5a89b2a70acab7453bbd17c4e91223ab0
Thomas <thomascode92@hotmail.com>
2025-02-04 21:20:55 +0100

    Display Line Numbers (with Catppuccin Colors)

--- private_dot_config/nvim/lua/config/vim.lua
+++ private_dot_config/nvim/lua/config/vim.lua
@@ -1,4 +1,10 @@
+-- Indentations
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Line numbers
+vim.opt.cursorline = true
+vim.cmd("set number")
+vim.cmd("set relativenumber")
--- private_dot_config/nvim/lua/plugins/catppuccin.lua
+++ private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -4,6 +4,15 @@ return {
   priority = 1000,
   flavour = "macchiato",
   config = function()
+    require("catppuccin").setup({
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.overlay1 }, -- Normal line numbers
+          CursorLineNr = { fg = colors.green }, -- Highlighted line number (current line)
+        }
+      end,
+    })
+
     vim.cmd.colorscheme("catppuccin")
   end,
 }
