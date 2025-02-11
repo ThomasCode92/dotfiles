4815bddba8f74017dbd039198059435add8bd197
Thomas <108367944+ThomasCode92@users.noreply.github.com>
2025-02-11 12:50:36 +0100

    Enhanced Neovim IDE Capabilities
    * Use 'nvim-tree' for File Explorer
    
    * Show Available Keymaps
    
    * Improve 'telescope' Configuration
    
    * Another Greeter with 'alpha'
    
    * Setup Automated Session Manager
    
    * Improve Status and Tab Bar
    
    * Small UI Improvement with 'dressing'
    
    * Improve the 'treesitter' Setup
    
    * Show Indentation

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,52 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require('nvim-treesitter.configs')
-    config.setup({
-      ensure_installed = { "lua", "javascript" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+
+      treesitter.setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        autotag = { enable = true },
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "vimdoc",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+    lazy = true,
+    event = "VeryLazy",
+  },
 }
