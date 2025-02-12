185df116b4a2ef8af90be84b5f1f30e68ff8e65f
Thomas <thomascode92@hotmail.com>
2025-02-12 21:45:50 +0100

    Setup Formatting & Linting

--- private_dot_config/nvim/lua/plugins/lsp/mason.lua
+++ private_dot_config/nvim/lua/plugins/lsp/mason.lua
@@ -2,10 +2,12 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
     mason.setup({
@@ -27,6 +29,14 @@ return {
         "tailwindcss",
         "lua_ls",
         "emmet_ls",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "eslint_d", -- eslint language server
       },
     })
   end,
