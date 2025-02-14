bcc520fdc50e932bf4c3d2bfcbe7342cb9aecfe4
Thomas <thomascode92@hotmail.com>
2025-02-14 07:56:18 +0100

    Improve Linting and Formatting

--- private_dot_config/nvim/lua/plugins/linting.lua
+++ private_dot_config/nvim/lua/plugins/linting.lua
@@ -11,17 +11,36 @@ return {
       typescriptreact = { "eslint_d" },
     }
 
+    local function find_nearest_node_modules_dir()
+      local current_dir = vim.fn.expand("%:p:h") -- current buffer dir
+      while current_dir ~= "/" do
+        if vim.fn.isdirectory(current_dir .. "/node_modules") == 1 then
+          return current_dir
+        end
+        current_dir = vim.fn.fnamemodify(current_dir, ":h")
+      end
+      return nil
+    end
+
+    local function get_root_dir()
+      local original_cwd = vim.fn.getcwd()
+      local node_modules_dir = find_nearest_node_modules_dir()
+      return node_modules_dir or original_cwd
+    end
+
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
-        lint.try_lint()
+        local root = get_root_dir()
+        lint.try_lint(nil, { cwd = root })
       end,
     })
 
     vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
+      local root = get_root_dir()
+      lint.try_lint(nil, { cwd = root })
     end, { desc = "Trigger linting for current file" })
   end,
 }
