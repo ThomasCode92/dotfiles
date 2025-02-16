42a891355f6e9a1eff84b0d6b28639e1f64fd5b2
Thomas <thomascode92@hotmail.com>
2025-02-16 21:20:44 +0100

    Add Custom Settings for 'fzf'

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fastfetch
+    
 end
 
 export PATH="~/.local/bin:$PATH"
@@ -10,3 +11,8 @@ source ~/.asdf/asdf.fish
 
 export EDITOR=nvim
 export VISUAL=nvim
+
+# Custom settings for fzf.fish
+set --export -gx $EDITOR "nvim" # or "vim", or "code", etc.
+set --export fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+set fzf_fd_opts --hidden --max-depth 3
