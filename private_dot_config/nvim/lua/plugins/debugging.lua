9474b55ba59ee0c0b3dc0004c28d2c0993bba897
Thomas <thomascode92@hotmail.com>
2025-02-20 20:14:09 +0100

    Configure Debugger for JS & TS

--- private_dot_config/nvim/lua/plugins/debugging.lua
+++ private_dot_config/nvim/lua/plugins/debugging.lua
@@ -1,11 +1,65 @@
+--- Gets a path to a package in the Mason registry.
+--- Prefer this to `get_package`, since the package might not always be
+--- available yet and trigger errors.
+---@param pkg string
+---@param path? string
+local function get_pkg_path(pkg, path)
+  pcall(require, "mason")
+  local root = vim.env.MASON or (vim.fn.stdpath("data") .. "/mason")
+  path = path or ""
+  local ret = root .. "/packages/" .. pkg .. "/" .. path
+  return ret
+end
+
 return {
   "mfussenegger/nvim-dap",
   dependencies = {
     "rcarriga/nvim-dap-ui",
+    {
+      "microsoft/vscode-js-debug",
+      opt = true,
+      run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+    },
   },
   config = function()
     local dap = require("dap")
     local dapui = require("dapui")
+
+    local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        args = {
+          get_pkg_path("js-debug-adapter", "/js-debug/src/dapDebugServer.js"),
+          "${port}",
+        },
+      },
+    }
+
+    for _, language in ipairs(js_based_languages) do
+      dap.configurations[language] = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach",
+          processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
+        },
+      }
+    end
+
+    dapui.setup()
 
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
