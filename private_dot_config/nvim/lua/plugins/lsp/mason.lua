08ce1f5cbd9b0e720a2fc163cacf0988c1a26ae1
Thomas <thomascode92@hotmail.com>
2025-02-28 16:49:39 +0100

    Use 'vtsls' instead of 'ts_ls'

--- private_dot_config/nvim/lua/plugins/lsp/mason.lua
+++ private_dot_config/nvim/lua/plugins/lsp/mason.lua
@@ -23,7 +23,8 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "ts_ls",
+        -- "ts_ls",
+        "vtsls",
         "html",
         "cssls",
         "tailwindcss",
