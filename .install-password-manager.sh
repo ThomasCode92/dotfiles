85b46eeac7c55ffa5141d807b4a4a90bbd255abd
Thomas <thomascode92@hotmail.com>
2025-03-11 13:28:42 +0100

    Update Install Script

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -3,6 +3,7 @@
 # exit immediately if password-manager-binary is already in $PATH
 type pass >/dev/null 2>&1 && exit
 
+# install password manager
 case "$(uname -s)" in
 Linux)
   sudo apt install pass
@@ -12,3 +13,7 @@ Linux)
   exit 1
   ;;
 esac
+
+# init password store repository
+git clone git@github.com:ThomasCode92/password-store.git .password-store
+pass init $PASS_GPG_KEY
