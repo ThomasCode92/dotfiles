e218e2928eac9e7c020d5f048ba2076b6ef30ecf
Thomas <thomascode92@hotmail.com>
2025-03-14 13:17:44 +0100

    Config Updates for 'fish' and 'starship'

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -32,7 +32,7 @@ set cyan "#94E2D5"          # Teal
 
 # --- setup fzf options ---
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
-export FZF_DEFAULT_OPTS="--color=fg:$fg,bg:$bg,hl:$purple,fg+:$fg,bg+:$bg_highlight,hl+:$purple,info:$blue,prompt:$cyan,pointer:$cyan,marker:$cyan,spinner:$cyan,header:$cyan --style=full --height=40% --tmux=80% --layout=reverse --border=top --preview 'bat -n --color=always --line-range :500 {}'"
+export FZF_DEFAULT_OPTS="--color=fg:$fg,bg:$bg,hl:$purple,fg+:$fg,bg+:$bg_highlight,hl+:$purple,info:$blue,prompt:$cyan,pointer:$cyan,marker:$cyan,spinner:$cyan,header:$cyan --style=full --height=40% --tmux=80% --layout=reverse --preview 'bat -n --color=always --line-range :500 {}'"
 set -g FZF_CTRL_T_COMMAND "command fd -L --type f --hidden --follow --exclude .git . \$dir"
 export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
 export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
@@ -40,7 +40,7 @@ export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
 # --- custom settings for fzf.fish ---
 set -gx $EDITOR "nvim" # or "vim", or "code", etc.
 set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
-set fzf_fd_opts --hidden --max-depth 2
+set fzf_fd_opts --hidden --max-depth 5
 set fzf_preview_dir_cmd eza --all --color=always --icons=always
 
 # --- shell wrapper for Yazi ---
