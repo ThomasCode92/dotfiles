a7cb25c5064dba72894aabbb063164c910c83c4c
Thomas <thomascode92@hotmail.com>
2025-03-21 14:47:40 +0100

    Update Debugging Config in Neovim

--- private_dot_config/nvim/lua/plugins/debugging.lua
+++ private_dot_config/nvim/lua/plugins/debugging.lua
@@ -20,6 +20,8 @@ return {
       opt = true,
       run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
     },
+    -- virtual text for the debugger
+    { "theHamsta/nvim-dap-virtual-text", opts = {} },
   },
   config = function()
     local dap = require("dap")
@@ -29,12 +31,33 @@ return {
     local js_based_languages = { "typescript", "javascript", "typescriptreact" }
     local debugger_path = get_pkg_path("js-debug-adapter", "/js-debug/src/dapDebugServer.js")
 
+    local dap_icons = {
+      Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+      Breakpoint = " ",
+      BreakpointCondition = " ",
+      BreakpointRejected = { " ", "DiagnosticError" },
+      LogPoint = ".>",
+    }
+
     -- Setup DAP config by VS Code launch.json file
     local dap_vscode = require("dap.ext.vscode")
     local json = require("plenary.json")
     ---@diagnostic disable-next-line: duplicate-set-field
     dap_vscode.json_decode = function(str)
+      -- replace chrome with pwa-chrome
+      str = str:gsub('"type": "chrome"', '"type": "pwa-chrome"')
       return vim.json.decode(json.json_strip_comments(str, {}))
+    end
+
+    vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+    -- Set icons for DAP
+    for name, sign in pairs(dap_icons) do
+      sign = type(sign) == "table" and sign or { sign }
+      vim.fn.sign_define(
+        "Dap" .. name,
+        { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+      )
     end
 
     -- Configure JS/TS adapters
@@ -107,22 +130,32 @@ return {
 
     dapui.setup()
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+    -- stylua: ignore start
+    dap.listeners.before.attach.dapui_config = function() dapui.open() end
+    dap.listeners.before.launch.dapui_config = function() dapui.open() end
+    dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+    dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+    -- stylua: ignore end
 
     local keymap = vim.keymap
 
-    keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
     keymap.set("n", "<Leader>dc", dap.continue, { desc = "Continue" })
+    keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+    keymap.set("n", "<leader>dc", dap.continue, { desc = "Run/Continue" })
+    keymap.set("n", "<leader>dC", dap.run_to_cursor, { desc = "Run to Cursor" })
+    keymap.set("n", "<leader>dg", dap.goto_, { desc = "Go to Line (No Execute)" })
+    keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
+    keymap.set("n", "<leader>dj", dap.down, { desc = "Down" })
+    keymap.set("n", "<leader>dk", dap.up, { desc = "Up" })
+    keymap.set("n", "<leader>dl", dap.run_last, { desc = "Run Last" })
+    keymap.set("n", "<leader>do", dap.step_out, { desc = "Step Out" })
+    keymap.set("n", "<leader>dO", dap.step_over, { desc = "Step Over" })
+    keymap.set("n", "<leader>dP", dap.pause, { desc = "Pause" })
+    keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Toggle REPL" })
+    keymap.set("n", "<leader>ds", dap.session, { desc = "Session" })
+    keymap.set("n", "<leader>dt", dap.terminate, { desc = "Terminate" })
+    keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle DAP UI" })
+    -- stylua: ignore
+    keymap.set("n", "<leader>dw", function() require("dap.ui.widgets").hover() end, { desc = "Widgets" })
   end,
 }
