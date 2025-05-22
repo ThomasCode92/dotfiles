77e23fdbb1083a6a2692ee2f10123a4fc959969d
Thomas <thomascode92@hotmail.com>
2025-05-22 09:04:31 +0200

    Switch CopilotChat for Avante

---
+++ private_dot_config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,50 @@
+--- This is taken from a PR of the LazuVim Project, this can be used to add Avante to the LazyVim setup
+--- it can be removed when the PR is merged. It uses Copilot as the default provider, instead of Claude.
+--- PR: https://github.com/LazyVim/LazyVim/pull/4440
+
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "ibhagwan/fzf-lua",
+    },
+    opts = {
+      -- Default configuration
+      hints = { enabled = false },
+
+      ---@alias AvanteProvider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+      provider = "copilot", -- Recommend using Claude
+      auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+
+      -- File selector configuration
+      --- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string
+      file_selector = {
+        provider = "fzf", -- Avoid native provider issues
+        provider_opts = {},
+      },
+    },
+    build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    lazy = true,
+    optional = true,
+    ft = function(_, ft)
+      vim.list_extend(ft, { "Avante" })
+    end,
+    opts = function(_, opts)
+      opts.file_types = vim.list_extend(opts.file_types or {}, { "Avante" })
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>a", group = "ai" },
+      },
+    },
+  },
+}
