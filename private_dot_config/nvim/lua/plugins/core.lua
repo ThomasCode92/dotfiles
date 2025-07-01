2179c4d73e2dd1f5cb1f8e04c538da805c870c85
Thomas <thomascode92@hotmail.com>
2025-07-01 08:17:30 +0200

    Adjust Catppuccin Theme for LazyVim

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -1,8 +1,20 @@
 return {
+  -- configure catppuccin colorscheme
+  {
+    "catppuccin/nvim",
+    opts = {
+      color_overrides = {
+        -- make 'mocha' colorscheme more dark
+        mocha = {
+          base = "#000000", -- background color
+          mantle = "#010101", -- file explorer background
+          crust = "#020202", -- top bar background
+        },
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    opts = { colorscheme = "catppuccin" },
   },
 }
