d96b605f85c64d4cddbc67b1f3c390bb3a8919b4
Thomas <thomascode92@hotmail.com>
2025-07-03 08:21:02 +0200

    Some Stylistic Improvements

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- change avante separators
+vim.api.nvim_set_hl(0, "AvanteSidebarWinSeparator", { fg = "#7f849c", bg = "#020202" })
+vim.api.nvim_set_hl(0, "AvanteSidebarWinHorizontalSeparator", { fg = "#7f849c", bg = "#020202" })
