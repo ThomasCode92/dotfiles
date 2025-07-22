be0a493e48c92d3d391636de223c75af35a14dff
Thomas <thomascode92@hotmail.com>
2025-07-22 13:06:49 +0200

    Switch Back to Blink (instead of 'nvim-cmp')

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,21 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpAccept",
+  callback = function(args)
+    local item = args.data.item
+    local new_text = item.textEdit and item.textEdit.newText or item.label
+
+    if item.source_name ~= "copilot" or new_text:find("\n") then
+      return -- Only handle single-line Copilot suggestions
+    end
+
+    -- Replace current line & place cursor at end of new line
+    local row = args.data.context.cursor[1] - 1 -- 0-based index
+    vim.api.nvim_buf_set_lines(0, row, row + 1, false, {})
+    vim.api.nvim_buf_set_lines(0, row, row, false, { new_text })
+    vim.api.nvim_win_set_cursor(0, { row + 1, #new_text })
+  end,
+})
--- private_dot_config/nvim/lua/plugins/blink.lua
+++ private_dot_config/nvim/lua/plugins/blink.lua
@@ -6,6 +6,24 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
+    completion = {
+      menu = { -- nvim-cmp style menu
+        draw = {
+          columns = {
+            { "label", "label_description", gap = 1 },
+            { "kind_icon", "kind" },
+            { "source_name" },
+          },
+          components = {
+            source_name = {
+              text = function(ctx)
+                return ctx.item.client_name or ctx.item.source_name
+              end,
+            },
+          },
+        },
+      },
+    },
     sources = {
       default = { "lsp", "avante", "path", "snippets", "buffer" },
       providers = {
