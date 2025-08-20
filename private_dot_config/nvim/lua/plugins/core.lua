66d1c47ab3b7ccd91d45cdc9ab2e37125f5b5a83
Thomas <thomascode92@hotmail.com>
2025-08-20 07:58:39 +0200

    Hack Bufferline Breaking Change (& Update Plugins)

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -22,13 +22,20 @@ return {
       config = function()
         require("colorizer").setup({
           filetypes = { "css", "typescriptreact", html = { mode = "foreground" } },
-          user_default_options = {
-            tailwind = "both",
-            tailwind_opts = { update_names = true },
-          },
+          user_default_options = { tailwind = "both", tailwind_opts = { update_names = true } },
         })
       end,
     },
+  },
+  {
+    "catppuccin",
+    optional = true,
+    opts = function()
+      -- HACK: Fix a breaking change in bufferline integration
+      -- Can be removed when the following PR is merged: https://github.com/LazyVim/LazyVim/pull/6354
+      local bufferline = require("catppuccin.groups.integrations.bufferline")
+      bufferline.get = bufferline.get or bufferline.get_theme
+    end,
   },
   { "m4xshen/hardtime.nvim", event = "BufReadPre", opts = {} }, -- break bad habits, enforce vim motions
   { "xvzc/chezmoi.nvim", init = function() end }, -- don't run chezmoi edit on file enter, see docs: https://www.lazyvim.org/extras/util/chezmoi#chezmoinvim
