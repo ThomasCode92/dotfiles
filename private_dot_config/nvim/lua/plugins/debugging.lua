d57a1338d0545f81889fc9e582ac898c85352993
Thomas <thomascode92@hotmail.com>
2025-10-13 19:21:01 +0200

    Refactor Debugging Setup
    - Mason v2 compliant
    - Better support for 'launch.json' files

--- private_dot_config/nvim/lua/plugins/debugging.lua
+++ private_dot_config/nvim/lua/plugins/debugging.lua
@@ -1,38 +1,59 @@
+-- Debug adapter configuration inspired by StevanFreeborn's Neovim setup
+-- Source: https://github.com/StevanFreeborn/nvim-config/blob/main/lua/plugins/debugging.lua
+
 return {
   {
     "mfussenegger/nvim-dap",
     opts = function()
       local dap = require("dap")
-      local mason_registry = require("mason-registry")
 
       local projectFolder = ""
       local file = vim.fn.expand("%:p")
       local cwd = vim.fn.getcwd():gsub([[-]], "%%-")
 
+      -- Path setup for JavaScript debug adapter installed via Mason
+      local dataPath = vim.fn.stdpath("data") -- Neovim's standard data directory (e.g., ~/.local/share/nvim on Linux)
+      local debugServerPath = dataPath .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js"
+
       if string.find(file, cwd .. "/apps/") then
         projectFolder = string.match(file, cwd .. "(.-/[^/]+)/src")
       end
 
-      if not dap.adapters["pwa-chrome"] then
-        dap.adapters["pwa-chrome"] = {
-          type = "server",
-          host = "localhost",
-          port = "${port}",
-          executable = {
-            command = "node",
-            args = {
-              mason_registry.get_package("js-debug-adapter"):get_install_path() .. "/js-debug/src/dapDebugServer.js",
-              "${port}",
+      for _, adapterType in ipairs({ "node", "chrome" }) do
+        local pwaAdapter = "pwa-" .. adapterType
+
+        if not dap.adapters[pwaAdapter] then
+          dap.adapters[pwaAdapter] = {
+            type = "server",
+            host = "localhost",
+            port = "${port}",
+            executable = {
+              command = "node",
+              args = { debugServerPath, "${port}" },
             },
-          },
-        }
+          }
+        end
+
+        -- this allow us to handle launch.json configurations
+        -- which specify type as "node" or "chrome"
+        dap.adapters[adapterType] = function(cb, config)
+          local nativeAdapter = dap.adapters[pwaAdapter]
+
+          config.type = pwaAdapter
+
+          if type(nativeAdapter) == "function" then
+            nativeAdapter(cb, config)
+          else
+            cb(nativeAdapter)
+          end
+        end
       end
 
       for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
         dap.configurations[language] = {
           {
             type = "pwa-node",
-            request = "Launch",
+            request = "launch",
             name = "launch file",
             program = "${file}",
             cwd = "${workspacefolder}",
