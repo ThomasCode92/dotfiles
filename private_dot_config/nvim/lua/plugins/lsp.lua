8ad9c412a8c94ad86fa580db1ffb0f4c61966bc9
Thomas <thomascode92@hotmail.com>
2025-10-16 15:25:01 +0200

    Update Tailwind CSS LSP Setup

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -23,12 +23,17 @@ return {
       root_markers = { ".git" },
     }),
     setup = {
+      -- Configure Tailwind CSS LSP to recognize class names in variable assignments
+      -- This enables IntelliSense for Tailwind classes stored in variables like:
+      -- const classes = "bg-red-500 text-white"
+      -- let class = "flex items-center"
       tailwindcss = function(_, opts)
         opts.settings = {
           tailwindCSS = {
             experimental = {
               classRegex = {
                 { "[cC]lasses\\s*\\+?=\\s*([^;]*);", "['\"`]([^'\"`]*)['\"`]" },
+                { "[cC]lass\\s*\\+?=\\s*([^;]*);", "['\"`]([^'\"`]*)['\"`]" },
               },
             },
           },
