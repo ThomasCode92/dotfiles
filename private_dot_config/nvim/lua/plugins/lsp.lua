5baf6c2e97f5154df5b0c369a20ce91d0a47b99a
Thomas <thomascode92@hotmail.com>
2025-10-20 21:41:08 +0200

    Update to LazyVim v15.10.1 🚀 (& Update Plugins)
    - Delete Custom 'nvim-dap' Setup
    - Delete Custom 'vtsls' LSP Setup
    - Refactor LSP Setup for Tailwind

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,44 +1,31 @@
--- inlay hints for typescript
-local inlay_hints = {
-  parameterNames = { enabled = "all" },
-  suppressWhenTypeMatchesName = false,
-}
-
 return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      vtsls = {
+      tailwindcss = {
         settings = {
-          typescript = { inlayHints = inlay_hints },
-        },
-      },
-      cssls = {},
-      emmet_language_server = {},
-    },
-    -- Configure 'cspell', make sure to install 'cspell-lsp' globally
-    vim.lsp.enable("cspell_ls"),
-    vim.lsp.config("cspell_ls", {
-      cmd = { "cspell-lsp", "--stdio" },
-      root_markers = { ".git" },
-    }),
-    setup = {
-      -- Configure Tailwind CSS LSP to recognize class names in variable assignments
-      -- This enables IntelliSense for Tailwind classes stored in variables like:
-      -- const classes = "bg-red-500 text-white"
-      -- let class = "flex items-center"
-      tailwindcss = function(_, opts)
-        opts.settings = {
           tailwindCSS = {
             experimental = {
+              -- This enables IntelliSense for Tailwind classes stored in variables like:
+              -- const classes = "bg-red-500 text-white"
+              -- let class = "flex items-center"
               classRegex = {
                 { "[cC]lasses\\s*\\+?=\\s*([^;]*);", "['\"`]([^'\"`]*)['\"`]" },
                 { "[cC]lass\\s*\\+?=\\s*([^;]*);", "['\"`]([^'\"`]*)['\"`]" },
               },
             },
           },
-        }
-      end,
+        },
+      },
+      cssls = {},
+      emmet_language_server = {},
     },
+
+    -- Configure 'cspell', make sure to install 'cspell-lsp' globally
+    vim.lsp.enable("cspell_ls"),
+    vim.lsp.config("cspell_ls", {
+      cmd = { "cspell-lsp", "--stdio" },
+      root_markers = { ".git" },
+    }),
   },
 }
