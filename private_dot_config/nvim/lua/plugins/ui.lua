54777f85c430da4aa2d2c837c422b4ecbf52af81
Thomas <thomascode92@hotmail.com>
2025-11-18 21:54:22 +0100

    Use HTML Icon and Set Prettier Parser for .gohtml Templates

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -3,10 +3,12 @@ return {
   opts = function()
     require("mini.icons").setup({})
 
-    local tsx_icon, tsx_hl = MiniIcons.get("extension", "tsx")
+    local html_icon, html_hl = MiniIcons.get("extension", "html")
+    local tsx_icon, _ = MiniIcons.get("extension", "tsx")
 
     return {
       extension = {
+        ["gohtml"] = { glyph = html_icon, hl = html_hl }, -- use html icon with orange color for *.gohtml files
         ["test.tsx"] = { glyph = tsx_icon, hl = "MiniIconsAzure" }, -- use tsx icon with azure color for *.test.tsx files
       },
     }
