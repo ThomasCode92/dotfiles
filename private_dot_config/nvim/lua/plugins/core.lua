0a39a79d50be06ba14e393144846676797f541c7
Thomas <thomascode92@hotmail.com>
2025-12-13 21:55:25 +0100

    Correct Syntax in Lua Table

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -1,14 +1,6 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
     keys = {
       { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
       { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
@@ -16,16 +8,16 @@ return {
       { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
-    {
-      "catgoose/nvim-colorizer.lua",
-      event = "BufReadPre",
-      config = function()
-        require("colorizer").setup({
-          filetypes = { "css", "typescriptreact", html = { mode = "foreground" } },
-          user_default_options = { tailwind = "both", tailwind_opts = { update_names = true } },
-        })
-      end,
-    },
+  },
+  {
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    config = function()
+      require("colorizer").setup({
+        filetypes = { "css", "typescriptreact", html = { mode = "foreground" } },
+        user_default_options = { tailwind = "both", tailwind_opts = { update_names = true } },
+      })
+    end,
   },
   { "m4xshen/hardtime.nvim", event = "BufReadPre", opts = {} }, -- break bad habits, enforce vim motions
   { "xvzc/chezmoi.nvim", init = function() end }, -- don't run chezmoi edit on file enter, see docs: https://www.lazyvim.org/extras/util/chezmoi#chezmoinvim
