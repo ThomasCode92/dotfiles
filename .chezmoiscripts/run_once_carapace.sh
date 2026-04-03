805827604949b05924c15df2a1ed489152819768
Thomas <thomascode92@hotmail.com>
2026-04-03 16:46:46 +0200

    Clean Up Install Scripts

--- .chezmoiscripts/run_once_carapace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# For more information, see
-# https://carapace-sh.github.io/carapace-bin/carapace-bin.html
-
-echo "deb [trusted=yes] https://apt.fury.io/rsteube/ /" | sudo tee /etc/apt/sources.list.d/fury.list >/dev/null
-
-sudo apt-get update && sudo apt-get install carapace-bin
