b61dd8937fbacb8de35f558a26fd269497ec51da
Thomas <thomascode92@hotmail.com>
2026-08-12 10:19:51 +0200

    feat: herdr, ollama and pi
    - install herdr and ollama, via homebrew
    - install pi agent, via script
    - todo will be fixed in later update

--- .chezmoiscripts/run_once_install-opencode.sh
+++ .chezmoiscripts/run_once_install-opencode.sh
@@ -7,3 +7,12 @@ if ! command -v opencode &>/dev/null; then
 else
   echo "⏭️ Opencode CLI already installed."
 fi
+
+if ! command -v pi &>/dev/null; then
+  echo "🤖 Installing the Pi CLI..."
+  # TODO: check node version (22.19.0 or higher) and install if not present
+  curl -fsSL https://pi.dev/install.sh | sh
+  echo "✅ Pi CLI installed successfully."
+else
+  echo "⏭️ Pi CLI already installed."
+fi
