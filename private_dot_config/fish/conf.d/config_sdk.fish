88886cf92492f3a85c970ed353455007931f9e47
Thomas <thomascode92@hotmail.com>
2026-08-19 20:40:40 +0200

    fix(fish): resolve SDKMAN_DIR correctly
    SDKMAN_DIR was set as a universal variable to the macOS Homebrew path,
    which never gets cleared on Linux. Resolve the path dynamically,
    falling back to $HOME/.sdkman and unsetting the variable when no
    installation exists.

--- private_dot_config/fish/conf.d/config_sdk.fish
+++ private_dot_config/fish/conf.d/config_sdk.fish
@@ -1,5 +1,7 @@
 if test -d /opt/homebrew/opt/sdkman-cli/libexec
     set -Ux SDKMAN_DIR /opt/homebrew/opt/sdkman-cli/libexec
+else if test -d $HOME/.sdkman
+    set -Ux SDKMAN_DIR $HOME/.sdkman
 else
-    # set -Ux SDKMAN_DIR $HOME/.sdkman
+    set -e -U SDKMAN_DIR
 end
