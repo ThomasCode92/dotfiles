d92d0533e92cd5ba4bda882560582259d98fca4f
Thomas <thomascode92@hotmail.com>
2026-08-21 08:25:55 +0200

    feat(fish): transient prompt
    Enable Starship's transient prompt for Fish.
    Extra: clean up config.fish

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -2,15 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# set the key bindings to use vi mode
-set -g fish_key_bindings fish_vi_key_bindings
+set -g fish_key_bindings fish_vi_key_bindings # vi mode
 
-# set a catppuccin theme
 echo y | fish_config theme save catppuccin-mocha >/dev/null 2>&1
 
-# source applications
 starship init fish | source
 zoxide init fish | source
+atuin init fish --disable-up-arrow | source
+
+# enable transience, using starship
+enable_transience
 
 # environment variables
 set -gx PATH ~/.local/bin $PATH
@@ -21,6 +22,3 @@ set -gx VISUAL nvim
 alias lz="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias lg="lazygit"
 alias ld="lazydocker"
-
-# search shell history with atuin
-atuin init fish --disable-up-arrow | source
